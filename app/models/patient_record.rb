d17b821d0a8a4ce0c86be2fde5b6435fd15fe299
Assane Diallo <assane@pando2.com>
2023-12-01 13:15:39 +0100

    Enumerize chest_pain_type, resting_electrocardiographic_results, slope, and thalassemia in PatientRecord model

--- app/models/patient_record.rb
+++ app/models/patient_record.rb
@@ -27,10 +27,11 @@ class PatientRecord < ApplicationRecord
 
   belongs_to :patient
 
-  enumerize :chest_pain_type, in: { typical_angina: 1, atypical_angina: 2, non_anginal_pain: 3, asymptomatic: 4 },
-                              predicates: true
+  # Enumerations
+  enumerize :chest_pain_type, in: {typical_angina: 1, atypical_angina: 2, non_anginal_pain: 3, asymptomatic: 4},
+                              predicates: true, scope: true
   enumerize :resting_electrocardiographic_results,
-            in: { normal: 0, stt_wave_abnormality: 1, left_ventricular_hypertrophy: 2 }, predicates: true
-  enumerize :slope, in: { upsloping: 1, flat: 2, downsloping: 3 }, predicates: true
-  enumerize :thalassemia, in: { normal: 3, fixed_defect: 6, reversible_defect: 7 }, predicates: true
+            in: {normal: 0, stt_wave_abnormality: 1, left_ventricular_hypertrophy: 2}, predicates: true, scope: true
+  enumerize :slope, in: {upsloping: 1, flat: 2, downsloping: 3}, predicates: true, scope: true
+  enumerize :thalassemia, in: {normal: 3, fixed_defect: 6, reversible_defect: 7}, predicates: true, scope: true
 end
--- db/migrate/20231130153552_create_patient_records.rb
+++ db/migrate/20231130153552_create_patient_records.rb
@@ -4,19 +4,20 @@
 class CreatePatientRecords < ActiveRecord::Migration[7.1]
   def change
     create_table :patient_records, id: :uuid do |t|
-      t.references :patient, null: false, foreign_key: true, type: :uuid
-      t.integer :chest_pain_type
+      t.references :patient, null: false, foreign_key: { to_table: :patients, on_delete: :cascade }, type: :uuid
+      t.integer :chest_pain_type, null: false
       t.float :resting_blood_pressure
       t.float :serum_cholesterol_level
-      t.float :fasting_blood_sugar
-      t.integer :resting_electrocardiographic_results
+      t.integer :fasting_blood_sugar
+      t.integer :resting_electrocardiographic_results, null: false
       t.integer :heart_rate_during_exercise
-      t.boolean :exercise_induced_angina
-      t.float :relative_to_rest_st_depression_induced_by_exercice
-      t.integer :slope
+      t.boolean :exercise_induced_angina, default: false
+      t.float :relative_to_rest_st_depression_induced_by_exercise
+      t.integer :slope, null: false
       t.integer :number_colored_major_vessels
-      t.integer :thalassemia
-      t.datetime :recorded_at
+      t.integer :thalassemia, null: false
+
+      t.datetime :recorded_at, null: false
 
       t.timestamps
     end
