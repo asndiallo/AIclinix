92e9dd1e3173c217f9b73ea3131436b5c17458aa
Assane Diallo <assane@pando2.com>
2023-12-04 21:06:23 +0100

    [#4] Refactor HeartDiseaseApiService to improve patient data transformation. This change is necessary to update the logic for transforming patient data and ensure accurate data is sent to the API for prediction.

--- app/services/heart_disease_api_service.rb
+++ app/services/heart_disease_api_service.rb
@@ -8,28 +8,46 @@ class HeartDiseaseApiService
   base_uri ENV.fetch('HEART_DISEASE_API_URL', nil)
 
   def self.predict(patient_record)
-    data = transform_patient_data(patient_record)
+    data = PatientDataTransformer.new(patient_record).transform
     response = post('/predict/', body: {data:}.to_json, headers: {'Content-Type' => 'application/json'})
     response.parsed_response if response.success?
   end
+end
 
-  def self.transform_patient_data(patient_record) # rubocop:disable Metrics/MethodLength
-    patient = patient_record.patient
+# Handles transformation of patient data for API consumption
+class PatientDataTransformer
+  attr_reader :patient_record, :patient
 
+  def initialize(patient_record)
+    @patient_record = patient_record
+    @patient = patient_record.patient
+  end
+
+  def transform # rubocop:disable Metrics/AbcSize
     {
       age:      patient.age,
       sex:      patient.sex_value,
-      cp:       patient_record.chest_pain_type_value, # Use the enumerize value method
+      cp:       patient_record.chest_pain_type_value,
       trestbps: patient_record.resting_blood_pressure,
       chol:     patient_record.serum_cholesterol,
-      fbs:      patient_record.fasting_blood_sugar > 120 ? 1 : 0,
+      fbs:      fasting_blood_sugar,
       restecg:  patient_record.resting_ecg_results_value,
       thalach:  patient_record.max_heart_rate_achieved,
-      exang:    patient_record.exercise_induced_angina ? 1 : 0,
+      exang:    exercise_induced_angina,
       oldpeak:  patient_record.st_depression,
       slope:    patient_record.st_slope_value,
       ca:       patient_record.number_colored_major_vessels,
       thal:     patient_record.thalassemia_value
     }
+  end
+
+  private
+
+  def fasting_blood_sugar
+    patient_record.fasting_blood_sugar > 120 ? 1 : 0
+  end
+
+  def exercise_induced_angina
+    patient_record.exercise_induced_angina ? 1 : 0
   end
 end
