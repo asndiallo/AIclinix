59d74aa45eb0a827964f2c3aa5a3b302030bc328
Assane Diallo <assane@pando2.com>
2023-12-04 13:01:36 +0100

    [#3] Refactor PatientRecord model and form to use new enumerations for chest_pain_type, resting_ecg_results, and st_slope. This change is necessary to improve the data representation and selection options in the patient record form.

--- app/models/patient_record.rb
+++ app/models/patient_record.rb
@@ -29,8 +29,8 @@ class PatientRecord < ApplicationRecord
   # Enumerations
   enumerize :chest_pain_type, in: {typical_angina: 1, atypical_angina: 2, non_anginal_pain: 3, asymptomatic: 4},
                               predicates: true, scope: true
-  enumerize :resting_electrocardiographic_results,
-            in: {normal: 0, stt_wave_abnormality: 1, left_ventricular_hypertrophy: 2}, predicates: true, scope: true
-  enumerize :slope, in: {upsloping: 1, flat: 2, downsloping: 3}, predicates: true, scope: true
+  enumerize :resting_ecg_results, in: {normal: 0, stt_wave_abnormality: 1, left_ventricular_hypertrophy: 2},
+                                  predicates: true, scope: true
+  enumerize :st_slope, in: {upsloping: 1, flat: 2, downsloping: 3}, predicates: true, scope: true
   enumerize :thalassemia, in: {normal: 3, fixed_defect: 6, reversible_defect: 7}, predicates: true, scope: true
 end
--- app/services/heart_disease_api_service.rb
+++ app/services/heart_disease_api_service.rb
@@ -23,13 +23,13 @@ class HeartDiseaseApiService
       sex:      patient.sex == 'male' ? 1 : 0,
       cp:       patient_record.chest_pain_type_value, # Use the enumerize value method
       trestbps: patient_record.resting_blood_pressure,
-      chol:     patient_record.serum_cholesterol_level,
+      chol:     patient_record.serum_cholesterol,
       fbs:      patient_record.fasting_blood_sugar,
-      restecg:  patient_record.resting_electrocardiographic_results_value,
-      thalach:  patient_record.heart_rate_during_exercise,
+      restecg:  patient_record.resting_ecg_results_value,
+      thalach:  patient_record.max_heart_rate_achieved,
       exang:    patient_record.exercise_induced_angina ? 1 : 0,
-      oldpeak:  patient_record.relative_to_rest_st_depression_induced_by_exercise,
-      slope:    patient_record.slope_value,
+      oldpeak:  patient_record.st_depression,
+      slope:    patient_record.st_slope_value,
       ca:       patient_record.number_colored_major_vessels,
       thal:     patient_record.thalassemia_value
     }
