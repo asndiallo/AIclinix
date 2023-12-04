014170e3ea5ef1c9955aba94b54f59c2dcdccbcf
Assane Diallo <assane@pando2.com>
2023-12-04 02:56:23 +0100

    Create DashboardController to handle dashboard related functionality such as index, show, edit, update, and destroy actions. Also, add the DashboardHelper module. This change is necessary to provide the necessary routes and functionality for the dashboard feature of the application.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Dashboard controller
+class DashboardController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
