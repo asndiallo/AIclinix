fdbfabe44a2225166f7b9d6f8e8401e9f53ce873
Assane Diallo <assane@pando2.com>
2024-01-17 20:59:35 +0100

    [#9] Add Font Awesome library for additional icons in the application. This patch includes the necessary link to import the Font Awesome CSS file from a CDN, allowing for the use of Font Awesome icons in the frontend.

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin 'apexcharts', to: 'https://ga.jspm.io/npm:apexcharts@3.41.0/dist/apexcharts.
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js'
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js'
+pin '@fortawesome/fontawesome-free', to: 'https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.5.1/js/fontawesome.js'
