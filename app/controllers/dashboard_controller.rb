ce23f8973b8c07eba549228aa48988ab5a9b874a
Assane Diallo <assane@pando2.com>
2024-01-17 20:09:05 +0100

    [#9] Refactor dashboard index view to improve patient summary and statistics display. This patch improves the readability and user experience of the dashboard by displaying the total number of patients and providing an overview of each patient, including their full name, age, sex, and birthdate. Additionally, if a patient has recorded data, the records are now displayed below the patient's information.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,13 @@
 
 # Dashboard controller
 class DashboardController < ApplicationController
-  def index; end
+  def index
+    @patients = current_user.patients
+    @total_patients = @patients.count
+    @patients_with_records = @patients.select { |patient| patient.patient_records.present? }
+    @patients_without_records = @patients - @patients_with_records
+    # ... any other data needed for the dashboard
+  end
 
   def show; end
 
